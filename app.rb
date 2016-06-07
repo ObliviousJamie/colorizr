6a7c6a9d2e7c6e9c2469e6a5638ec378710c3235
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-07 15:56:56 +0100

    Made file to demo changes

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'colorizr'
+
+#DEMO
+#
+#Creating colors
+String.create_colors
+#Example strings
+example = "example string"
+String.colors.each do |color|
+    puts example.send("#{color.to_s}")
+end
+#Sample colors
+String.sample_colors
+#Returns array of colors
+p String.colors
