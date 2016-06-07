cd7190332fdbc3e31b701c60fc3c2405f799a2ce
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-07 15:06:16 +0100

    Added metaprogramming to create color methods

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,11 +1,16 @@
-require 'colorizr'
 class String 
-    def initialize
-        super()
-        create_colors
-    end
+    #Hash map of valid colors
+    @colors = Hash["red", 31, "green", 32, "yellow", 33, "blue", 34, "pink", 35, "light_blue", 96, "white", 97,
+                  "light_grey", 37, "black", 30]
+
     def self.create_colors
         #Using instance methods to create colors
+        @colors.each do |(color,code)|
+            self.send(:define_method,"#{color}") do
+                "\e[#{code}m #{self} \e[0m"
+            end
+        end
+                
     end
     
     def colors
@@ -16,3 +21,8 @@ class String
         #Displays a sample output of colors
     end
 end
+
+String.create_colors
+redc = "example string"
+redc.red
+redc.blue
