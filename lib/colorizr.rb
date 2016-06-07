f787bc4b1bcd861bafeae4abba1318fe86cbebd4
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-07 15:59:37 +0100

    Added comments

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -3,10 +3,11 @@ class String
     @colors = Hash[:red, 31, :green, 32, :yellow, 33, :blue, 34, :pink, 35, :light_blue, 96, :white, 97,
                   :light_grey, 37, :black, 30]
 
+    #Loops through array of colors and
+    #adds color code to self
     def self.create_colors
         #Using instance methods to create colors
         @colors.each do |(color,code)|
-            puts color.to_s
             self.send(:define_method,"#{color.to_s}") do
                 "\e[#{code}m#{self}\e[0m"
             end
@@ -14,11 +15,14 @@ class String
                 
     end
     
+    #Returns an array of colors
     def self.colors
         #Displays an array of colors
         @colors.keys.to_a
     end
 
+    #Loops and calls all color methods in the format
+    #This is "color" with the correct color assigned
     def self.sample_colors
         #Displays a sample output of colors
         @colors.each_key do |color|
@@ -27,10 +31,3 @@ class String
         end
     end
 end
-
-String.create_colors
-redc = "example string"
-puts redc.red
-puts redc.blue
-String.sample_colors
-p String.colors
